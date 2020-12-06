eadb1588f174a32ce12b5227fc94faa7a854e6f9
ahkui <14049597+ahkui@users.noreply.github.com>
2020-12-06 23:50:25 +0800

    Update PlayerDropSkull.py

--- PlayerDropSkull.py
+++ PlayerDropSkull.py
@@ -8,28 +8,33 @@ P = 0.5
 
 
 def on_death_message(server, message):
-	if random.random() <= P:
-		player = message.split(' ')[0]
-		api = server.get_plugin_instance('PlayerInfoAPI')
-		data = api.getPlayerInfo(server, player)
-		x, y, z = data['Pos']
-		dim = data['Dimension']
-		dim_mapping = {
-			0: 'overworld',
-			-1: 'the_nether',
-			1: 'the_end'
-		}
-		prefix = 'execute in minecraft:{} run '.format(dim_mapping[dim])
-		nbt = json.dumps({
-			'Item': {
-				'id': 'minecraft:player_head',
-				'Count': 1,
-				'tag': {
-					'SkullOwner': {
-						'Name': player
-					}
-				}
-			}
-		})
-		command = prefix + 'summon item {} {} {} {}'.format(x, y, z, nbt)
-		server.execute(command)
+    if random.random() <= P:
+        player = message.split(' ')[0]
+
+        PlayerInfoAPI = server.get_plugin_instance('PlayerInfoAPI')
+        pos = PlayerInfoAPI.getPlayerInfo(server, player, path='Pos')
+        x = int(pos[0])
+        y = int(pos[1]) + 1
+        z = int(pos[2])
+        dim = PlayerInfoAPI.getPlayerInfo(server, player, path='Dimension')
+        dim_tran = {
+            0: 'minecraft:overworld',
+            -1: 'minecraft:the_nether',
+            1: 'minecraft:the_end',
+        }
+
+        prefix = 'execute in {} run '.format(dim_tran.get(dim, dim))
+        nbt = json.dumps({
+            'Item': {
+                'id': 'minecraft:player_head',
+                'Count': 1,
+                'tag': {
+                    'SkullOwner': {
+                        'Name': player
+                    }
+                }
+            }
+        })
+        command = prefix + 'summon item {} {} {} {}'.format(x, y, z, nbt)
+
+        server.execute(command)
