e3d0606b447e471cdc7492ef99e4bb69a9a703f7
泉户琥珀 <42205627+mitokohaku@users.noreply.github.com>
2021-01-15 23:16:31 +0800

    Update PlayerDropSkull.py

--- PlayerDropSkull.py
+++ PlayerDropSkull.py
@@ -4,22 +4,24 @@ import random
 
 # the possibility for a player to drop his skull
 # the value should be from 0.0 to 1.0
-P = 0.5
+P = 0.2
 
 
 def on_death_message(server, message):
 	if random.random() <= P:
 		player = message.split(' ')[0]
 		api = server.get_plugin_instance('PlayerInfoAPI')
-		data = api.getPlayerInfo(server, player)
-		x, y, z = data['Pos']
-		dim = data['Dimension']
+		x, y, z = api.getPlayerInfo(server, player, path='Pos')
+		dim = api.getPlayerInfo(server, player, path='Dimension')
 		dim_mapping = {
-			0: 'overworld',
-			-1: 'the_nether',
-			1: 'the_end'
-		}
-		prefix = 'execute in minecraft:{} run '.format(dim_mapping[dim])
+            0: 'minecraft:overworld',
+            -1: 'minecraft:the_nether',
+            1: 'minecraft:the_end',
+            'minecraft:overworld': 'minecraft:overworld',
+            'minecraft:the_nether': 'minecraft:the_nether',
+            'minecraft:the_end': 'minecraft:the_end'
+        }
+		prefix = 'execute in {} run '.format(dim_mapping[dim])
 		nbt = json.dumps({
 			'Item': {
 				'id': 'minecraft:player_head',
